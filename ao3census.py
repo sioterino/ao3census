2f3ee40790117986d53deaff871c93c7a4abea9c
sioterino <sofialvestoreti@gmail.com>
2024-12-12 16:35:44 -0300

    done with data frame, error pages

--- ao3census.py
+++ ao3census.py
@@ -12,13 +12,36 @@ from datetime import datetime
 
 # from InquirerPy import inquirer as iq
 
-all_titles: list = []
-all_authors: list = []
-all_fandoms: list = []
+all_titles: list[str] = []
+all_authors: list[str] = []
+all_fandoms: list[str] = []
 
-df_works: DataFrame = pd.DataFrame(columns=['title', 'authors', 'fandoms', 'rating', 'warnings', 'orientation',
-                                            'status', 'update', 'tags', 'language', 'words', 'chapters',
-                                            'collections', 'comments', 'kudos', 'bookmarks', 'hits'])
+all_ratings: list[str] = []
+all_warnings: list[str] = []
+all_orientations: list[str] = []
+all_status: list[str] = []
+
+all_updates: list = []
+
+all_ships: list[str] = []
+all_chars: list[str] = []
+all_tags: list[str] = []
+
+all_languages: list[str] = []
+all_words: list[int] = []
+all_chapters: list[int] = []
+all_collections: list[int] = []
+all_comments: list[int] = []
+all_kudos: list[int] = []
+all_bookmarks: list[int] = []
+all_hits: list[int] = []
+
+all_series: list[str] = []
+
+df_works: DataFrame = pd.DataFrame(columns=['title', 'authors', 'crossovers', 'rating', 'warnings',
+                                            'orientations', 'status', 'update', 'ships', 'characters',
+                                            'tags', 'language', 'words', 'chapters', 'collections',
+                                            'comments', 'kudos', 'bookmarks', 'hits', 'series'])
 
 
 # requests access to a fandom page on ao3
@@ -27,10 +50,10 @@ def access_page(fandom: str, category: str, page: int, is_session: bool, session
     url: str = f'https://archiveofourown.org/tags/{fandom}%20({category})/works?page={page}'
     # sends a request message get to the server
     if is_session:
-        print("\nusing session")
+        # print("\nusing session")
         response: Response = session.get(url)
     else:
-        print("using request")
+        # print("using request")
         response: Response = requests.get(url)
 
     # checks status_code of the page and handles possible outcomes
@@ -43,9 +66,10 @@ def access_page(fandom: str, category: str, page: int, is_session: bool, session
 def checks_sc(status_code):
     match status_code:
         case 200:
-            print("sc: OK")
+            # print("sc: OK")
+            pass
         case 404:
-            print("sc: not found")
+            # print("sc: not found")
             exit(1)
         case __:
             print(f"unexpected sc: {status_code}")
@@ -77,11 +101,18 @@ def fandom_name(name: str) -> str:
         return name
 
 
+def revert_fandom_name(name: str) -> str:
+    if '%20' in name:
+        return name.replace("%20", "_")
+    else:
+        return name
+
+
 # gets the number of pages said fandom page has
 def get_number_of_pages(soup: BeautifulSoup) -> int:
     a_tag: ResultSet = soup.find('ol', class_='pagination actions').findAll('a')
 
-    a_text: list = []
+    a_text: list[int] = []
     for a in a_tag:
         if not bool(re.search(r'[a-zA-Z]', a.text)):
             a_text.append(int(a.text))
@@ -89,35 +120,124 @@ def get_number_of_pages(soup: BeautifulSoup) -> int:
     return a_text[-1]
 
 
+def get_original_fandom_name(soup: BeautifulSoup) -> str:
+    return soup.find('h2', class_='heading').find('a', class_='tag').text
+
+
 # parses html into a dataframe
-def parse_into_df(soup: BeautifulSoup) -> dict:
-    # parses all works into a list
+def parse_into_df(soup: BeautifulSoup, fandom_name: str) -> dict:
+
     work_list: list = soup.select('ol.work.index.group li.work.blurb.group')
 
     global all_titles
-    # parses all TITLES from given page into a list
-    page_titles: list = Parse.get_titles(work_list)
-    # saves all titles from given page into a global list of titles
+    page_titles: list[str] = Parse.get_titles(work_list)
     for title in page_titles:
         all_titles.append(title)
 
     global all_authors
-    # parses all AUTHORS from given page into a list
-    page_authors: list = Parse.get_authors(work_list)
-    # saves all authors from given page into a global list of authors
+    page_authors: list[str] = Parse.get_authors(work_list)
     for author in page_authors:
         all_authors.append(author)
 
     global all_fandoms
-    page_fandoms: list = Parse.get_fandoms(work_list)
+    page_fandoms: list[str] = Parse.get_fandoms(work_list, fandom_name)
     for fandom in page_fandoms:
         all_fandoms.append(fandom)
 
-    return {'title': all_titles, 'authors': all_authors, 'fandoms': all_fandoms}
+    global all_ratings
+    page_ratings: list[str] = Parse.get_ratings(work_list)
+    for rating in page_ratings:
+        all_ratings.append(rating)
+
+    global all_warnings
+    page_warnings: list[str] = Parse.get_warnings(work_list)
+    for warning in page_warnings:
+        all_warnings.append(warning)
+
+    global all_orientations
+    page_orientations: list[str] = Parse.get_orientations(work_list)
+    for orientation in page_orientations:
+        all_orientations.append(orientation)
+
+    global all_status
+    page_status: list[str] = Parse.get_status(work_list)
+    for stat in page_status:
+        all_status.append(stat)
+
+    global all_updates
+    page_updates: list = Parse.get_updates(work_list)
+    for update in page_updates:
+        all_updates.append(update)
+
+    global all_ships
+    page_ships: list[str] = Parse.get_ships(work_list)
+    for ship in page_ships:
+        all_ships.append(ship)
+
+    global all_chars
+    page_chars: list[str] = Parse.get_chars(work_list)
+    for char in page_chars:
+        all_chars.append(char)
+
+    global all_tags
+    page_tags: list[str] = Parse.get_freeform_tags(work_list)
+    for tag in page_tags:
+        all_tags.append(tag)
+
+    global all_languages
+    page_languages: list[str] = Parse.get_language(work_list)
+    for lang in page_languages:
+        all_languages.append(lang)
+
+    global all_words
+    page_words: list[int] = Parse.get_words(work_list)
+    for words in page_words:
+        all_words.append(words)
+
+    global all_chapters
+    page_chapters: list[int] = Parse.get_chapters(work_list)
+    for chaps in page_chapters:
+        all_chapters.append(chaps)
+
+    global all_collections
+    page_collections: list[int] = Parse.get_collections(work_list)
+    for collect in page_collections:
+        all_collections.append(collect)
+
+    global all_comments
+    page_comments: list[int] = Parse.get_comments(work_list)
+    for comments in page_comments:
+        all_comments.append(comments)
+
+    global all_kudos
+    page_kudos: list[int] = Parse.get_kudos(work_list)
+    for kud in page_kudos:
+        all_kudos.append(kud)
+
+    global all_bookmarks
+    page_bookmarks: list[int] = Parse.get_bookmarks(work_list)
+    for marks in page_bookmarks:
+        all_bookmarks.append(marks)
+
+    global all_hits
+    page_hits: list[int] = Parse.get_hits(work_list)
+    for hit in page_hits:
+        all_hits.append(hit)
+
+    global all_series
+    page_series: list[str] = Parse.get_series(work_list)
+    for part in page_series:
+        all_series.append(part)
+
+    return {'title': all_titles, 'authors': all_authors, 'crossovers': all_fandoms, 'rating': all_ratings,
+            'warnings': all_warnings, 'orientations': all_orientations, 'status': all_status, 'update': all_updates,
+            'ships': all_ships, 'characters': all_chars, 'tags': all_tags, 'language': all_languages,
+            'words': all_words, 'chapters': all_chapters, 'collections': all_collections, 'comments': all_comments,
+            'kudos': all_kudos, 'bookmarks': all_bookmarks, 'hits': all_hits, 'series': all_series}
 
 
 def save_dataframe(dict_works: dict, fandom: str):
-    file_name = f"./results/works_{fandom}_{datetime.now().strftime('%Y%m%d')}.csv"
+    file_name: str = f"./results/works_{revert_fandom_name(fandom)}_{datetime.now().strftime('%Y%m%d')}.csv"
     global df_works
     df_works = pd.concat([df_works, pd.DataFrame(dict_works)], ignore_index=True)
     df_works.to_csv(file_name, index=False, encoding="utf-8")
@@ -128,22 +248,21 @@ def main():
     fandom: str = fandom_name('le sserafim')
     band: str = 'Band'
 
-    html = get_html_of_page(fandom=fandom, category=band, is_session=False, session=None)
-    pages = get_number_of_pages(html)
+    html: BeautifulSoup = get_html_of_page(fandom=fandom, category=band, is_session=False, session=None)
+    pages: int = get_number_of_pages(html)
+    original_fandom_tag: str = get_original_fandom_name(html)
 
-    session = requests.Session()
-    pbar = tqdm((range(1, 2)), dynamic_ncols=True, unit="works", leave=False)
+    session: Session = requests.Session()
+    pbar: tqdm = tqdm((range(1, pages - 1)), dynamic_ncols=True, unit="works", leave=False)
     for page in pbar:
         pbar.set_description(f'Fetching page {page}')
-        soup = get_html_of_page(fandom=fandom, category=band, is_session=True, session=session, page=page)
-        dict_works: dict = parse_into_df(soup)
-        print("\n\n\n")
-        print(dict_works)
-        # save_dataframe(dict_works, fandom)
+        soup: BeautifulSoup = get_html_of_page(fandom=fandom, category=band, is_session=True, session=session, page=page)
+        dict_works: dict = parse_into_df(soup, original_fandom_tag)
+        save_dataframe(dict_works, fandom)
         sleep(random.randrange(6, 10))
 
     session.close()
-    print("done")
+    print("\n\n\ndone")
 
 
 if __name__ == '__main__':
--- parse.py
+++ parse.py
@@ -1,29 +1,189 @@
-from datetime import datetime
+from bs4 import BeautifulSoup, Tag
+
 
 class Parse:
-    def get_titles(work_list: list) -> list:
-        titles: list = []
+    def get_titles(work_list: list) -> list[str]:
+        titles: list[str] = []
         for work in work_list:
-            title: list = work.find('div', class_='header module').find('h4', class_='heading').find('a').text
+            title: str = work.find('div', class_='header module').find('h4', class_='heading').find('a').text
             titles.append(title)
         return titles
 
-    def get_authors(work_list: list) -> list:
+    def get_authors(work_list: list) -> list[str]:
         authors: list = []
         for work in work_list:
             author_tags: list = work.find('div', class_='header module').find('h4', class_='heading').find_all(rel="author")
-            author_names = [author.text for author in author_tags]
-            authors.append(author_names)
+            authors.append([author.text for author in author_tags])
         return authors
 
-    def get_fandoms(work_list: list) -> list:
+    def get_fandoms(work_list: list, fandom_name: str) -> list[str]:
         fandoms: list = []
         for work in work_list:
             fandom_tags: list = work.find('div', class_='header module').find('h5', class_='fandoms heading').find_all('a')
-            fandom_names = [fandom.text for fandom in fandom_tags]
-            fandoms.append(fandom_names)
-        print(fandoms)
-        return fandoms
+            fandoms.append([fandom.text for fandom in fandom_tags])
+        return remove_original_fandom(fandoms, fandom_name)
 
-if __name__ == '__main__':
-    print()
+
+    def get_ratings(work_list: list) -> list[str]:
+        ratings: list[str] = []
+        for work in work_list:
+            ratings.extend(get_required_tags(work)[0])
+        return ratings
+
+
+    def get_warnings(work_list: list) -> list[str]:
+        warnings: list[str] = []
+        for work in work_list:
+            warnings.append(get_required_tags(work)[1])
+        return warnings
+
+
+    def get_orientations(work_list: list) -> list[str]:
+        orientations: list[str] = []
+        for work in work_list:
+            orientations.append(get_required_tags(work)[2])
+        return orientations
+
+
+    def get_status(work_list: list) -> list[str]:
+        status: list[str] = []
+        for work in work_list:
+            status.extend(get_required_tags(work)[3])
+        return status
+
+
+    def get_updates(work_list: list) -> list[str]:
+        updates: list[str] = []
+        for work in work_list:
+            update: str = work.find('p', class_='datetime').text
+            updates.append(update)
+        return updates
+
+
+    def get_ships(work_list: list) -> list[str]:
+        ships: list = []
+        for work in work_list:
+            ship_tags: list = work.find('ul', class_='tags commas').find_all('li', class_='relationships')
+            ships.append([ship.text for ship in ship_tags])
+        return ships
+
+
+    def get_chars(work_list: list) -> list[str]:
+        chars: list = []
+        for work in work_list:
+            char_tags: list = work.find('ul', class_='tags commas').find_all('li', class_='characters')
+            chars.append([ship.text for ship in char_tags])
+        return chars
+
+
+    def get_freeform_tags(work_list: list) -> list[str]:
+        tags: list = []
+        for work in work_list:
+            tag_tags: list = work.find('ul', class_='tags commas').find_all('li', class_='freeforms')
+            tags.append([ship.text for ship in tag_tags])
+        return tags
+
+
+    def get_language(work_list: list) -> list[str]:
+        languages: list = []
+        for work in work_list:
+            language: str = work.find('dl', class_='stats').find('dd', class_='language').text
+            languages.append(language)
+        return languages
+
+
+    def get_words(work_list: list) -> list[int]:
+        words: list[int] = []
+        for work in work_list:
+            word: str = work.find('dl', class_='stats').find('dd', class_='words').text \
+                if work.find('dl', class_='stats').find('dd', class_='words') else '0'
+            words.append(str2int(word))
+        return words
+
+
+    def get_chapters(work_list: list) -> list[int]:
+        chapters: list[int] = []
+        for work in work_list:
+            chaps: str = work.find('dl', class_='stats').find('dd', class_='chapters').text \
+                if work.find('dl', class_='stats').find('dd', class_='chapters') else '0'
+            chapters.append(str2int(chaps.split('/')[0]))
+        return chapters
+
+
+    def get_collections(work_list: list) -> list[int]:
+        collections: list[int] = []
+        for work in work_list:
+            collect: str = work.find('dl', class_='stats').find('dd', class_='collections').text \
+                if work.find('dl', class_='stats').find('dd', class_='collections') else '0'
+            collections.append(str2int(collect))
+        return collections
+
+
+    def get_comments(work_list: list) -> list[int]:
+        comments: list[int] = []
+        for work in work_list:
+            comment: str = work.find('dl', class_='stats').find('dd', class_='words').text \
+                if work.find('dl', class_='stats').find('dd', class_='words') else '0'
+            comments.append(str2int(comment))
+        return comments
+
+
+    def get_kudos(work_list: list) -> list[int]:
+        kudos: list[int] = []
+        for work in work_list:
+            kud: str = work.find('dl', class_='stats').find('dd', class_='kudos').text \
+                if work.find('dl', class_='stats').find('dd', class_='kudos') else '0'
+            kudos.append(str2int(kud))
+        return kudos
+
+
+    def get_bookmarks(work_list: list) -> list[int]:
+        bookmarks: list[int] = []
+        for work in work_list:
+            mark: str = work.find('dl', class_='stats').find('dd', class_='bookmarks').text \
+                if work.find('dl', class_='stats').find('dd', class_='bookmarks') else '0'
+            bookmarks.append(str2int(mark))
+        return bookmarks
+
+
+    def get_hits(work_list: list) -> list[int]:
+        hits: list[int] = []
+        for work in work_list:
+            hit: str = work.find('dl', class_='stats').find('dd', class_='hits').text \
+                if work.find('dl', class_='stats').find('dd', class_='hits') else '0'
+            hits.append(str2int(hit))
+        return hits
+
+
+    def get_series(work_list: list) -> list[str]:
+        series: list[str] = []
+        for work in work_list:
+            part: str = work.find('ul', class_='series').find('li').text \
+                if work.find('ul', class_='series') else ''
+            series.append(part.strip())
+        return series
+
+
+def remove_original_fandom(fandoms: list, fandom_name: str) -> list:
+    for tags in fandoms:
+        if fandom_name in tags:
+            tags.remove(fandom_name)
+    return fandoms
+
+
+def get_required_tags(work: BeautifulSoup):
+    a_tags: list = work.findAll('a', class_='help symbol question modal')
+    text: list = []
+    for a in a_tags:
+        span: str = a.find('span', class_='text').text
+        text.append([span])
+    return text
+
+
+def str2int(num: str) -> int:
+    if ',' in num:
+        return int(num.replace(",", ""))
+    elif '.' in num:
+        return int(num.replace(".", ""))
+    else:
+        return int(num)
